1ecf0596d2cba7fe294ae49d2fdd272bfb7f33df
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2016-07-11 16:39:24 +0200

    Made first two rectangles

--- library.js
+++ library.js
@@ -1,11 +1,20 @@
 // Decorator 
 var carlike = function(obj, loc) {
     obj.loc = loc;
-    obj.move = move;
+    obj.move = function(){ // even better
+    	obj.loc++;
+    }
     return obj;
 };
 
+var john = new carlike({},1);
+john.move();
+
 // delagates to the decorator, becomes a property of carlike
-var move = function() {
-    this.loc++;
-};
+// var move = function() {
+//     this.loc++; 
+// };
+
+// this refers to the object on the left of the dot at the time of running
+//    var john = carlike({},1);
+//    john.move();   <-- as in here, binded to John
